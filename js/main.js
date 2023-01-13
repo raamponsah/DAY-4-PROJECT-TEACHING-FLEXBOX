2c62e8a42d7f574d95ade784216e2bf12f934f32
Ralph <ramponsah026@st.ug.edu.gh>
2023-01-13 09:44:45 +0000

    updated third section

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// const body = document.querySelector('body');
+// body.style.background = "red"
+// body.style.color = "yellow"
+
+// const h3 = document.createElement('h3');
+// h3.innerText = "Hello World";
+
+// body.appendChild(h3);
+
+
+// confirm("Are you sure you want to delete this file?");
